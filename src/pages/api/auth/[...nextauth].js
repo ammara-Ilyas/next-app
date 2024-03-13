60ae0816619a9ae5dd2eb05c47d52e4ca0a20381
ammara-Ilyas <ismarailyas545@gmail.com>
2024-03-13 23:54:37 +0500

    Login page

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 import { Imprima } from "next/font/google";
 import GitHubProvider from "next-auth/providers/github";
 export const authOption = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     GitHubProvider({
       clientId: "bcfda3ae3f257dcd3374",
       clientSecret: "process.env.GITHUB_CLIENT_SECRET",
---
+++ src/pages/contact/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "../../../component/contact/login/Login";
+function index() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default index;
